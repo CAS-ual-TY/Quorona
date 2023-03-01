55054403541f60097021433b9454f12c3cce4d58
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-03-01 17:54:31 +0100

    Update to 1.19.3

--- src/main/java/de/cas_ual_ty/quorona/QuoronaItems.java
+++ src/main/java/de/cas_ual_ty/quorona/QuoronaItems.java
@@ -3,8 +3,9 @@ package de.cas_ual_ty.quorona;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,10 +15,19 @@ public class QuoronaItems
 {
     private static final DeferredRegister<Item> DEFERRED_REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, Quorona.MOD_ID);
     
-    public static final RegistryObject<Item> DEAD_BAT = DEFERRED_REGISTER.register("dead_bat", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().saturationMod(0.1F).effect(() -> new MobEffectInstance(MobEffects.HUNGER, 600, 0), 0.8F).meat().build())));
+    public static final RegistryObject<Item> DEAD_BAT = DEFERRED_REGISTER.register("dead_bat", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().saturationMod(0.1F).effect(() -> new MobEffectInstance(MobEffects.HUNGER, 600, 0), 0.8F).meat().build())));
     
     public static void register(IEventBus modBus)
     {
         DEFERRED_REGISTER.register(modBus);
+        modBus.addListener(QuoronaItems::creativeModeTabs);
+    }
+    
+    public static void creativeModeTabs(CreativeModeTabEvent.BuildContents event)
+    {
+        if(event.getTab() == CreativeModeTabs.FOOD_AND_DRINKS)
+        {
+            event.accept(DEAD_BAT);
+        }
     }
 }
